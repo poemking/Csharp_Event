1e0096d4512ebfeeae039069ba131c2b9c797e3b
cheng han,yang <poemofking@hotmail.com>
2016-07-28 23:49:19 +0800

    add project
    Signed-off-by: cheng han,yang <poemofking@gmail.com>

---
+++ WinProj_Event/WinProj_Event/Account.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinProj_Event
+{
+    class Account
+    {
+        public event EventHandler AccountInDeficit;
+        public event EventHandler AccountInCredit;
+
+        private double _balance;
+
+        public void Deposit(double amount) 
+        {
+            _balance += amount;
+
+            EventHandler handler = AccountInCredit;
+
+            if (handler != null) 
+            {
+                AccountEventArgs args = new AccountEventArgs(_balance);
+                handler(this, args);//this代表這個帳戶產生的資料 arg代表現在存款餘額傳入方法中
+            }
+
+        }
+
+        public void Withdraw(double amount) 
+        {
+            _balance -= amount;
+
+            EventHandler handler = AccountInDeficit;
+
+            if (handler != null)
+            {
+                AccountEventArgs args = new AccountEventArgs(_balance);
+                handler(this, args);//透過account最後金額跟最後存款餘額值傳入方法中
+            }
+        }
+    }
+
+    //Custom event argument Class.
+    public class AccountEventArgs : EventArgs
+    {
+        private double _balance;
+        public AccountEventArgs(double b) 
+        {
+            _balance = b;
+        }
+        public double Balance 
+        {
+            get { return _balance; }
+        }
+    }
+}
---
+++ WinProj_Event/WinProj_Event/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinProj_Event
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Account acc1 = new Account();
+            acc1.AccountInCredit += OnAccountInCredit;
+            acc1.AccountInDeficit += OnAccountInCredit;
+
+            acc1.Deposit(50);
+            acc1.Deposit(70);
+            acc1.Withdraw(100);
+
+                //Result
+              //Account in credit, new balance NT$50.00
+              //Account in credit, new balance NT$120.00
+              //Account in credit, new balance NT$20.00
+        }
+
+        void OnAccountInCredit(object sender, EventArgs args) 
+        {
+            AccountEventArgs e = (AccountEventArgs)args;
+            double balance = e.Balance;
+            Console.WriteLine("Account in credit, new balance {0:c}", balance);
+        }
+    }    
+}
